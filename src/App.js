9b46be6782f3b04fcc6802037a066a30737af7f3
Ryan Yeo <ryandc.yeo@gmail.com>
2021-12-04 00:20:27 -0800

    Milestone 3

--- src/App.js
+++ src/App.js
@@ -2,16 +2,6 @@ import React from 'react';
 import PostsDisplay from './PostsDisplay';
 import './App.css';
 
-// airtable configuration
-const Airtable = require('airtable');
-
-const airtableConfig = {
-  apiKey: process.env.REACT_APP_AIRTABLE_USER_KEY,
-  baseKey: process.env.REACT_APP_AIRTABLE_BASE_KEY,
-};
-const base = new Airtable({ apiKey: airtableConfig.apiKey })
-  .base(airtableConfig.baseKey);
-
 function App() {
   return (
     <>
--- src/PostsDisplay.js
+++ src/PostsDisplay.js
@@ -14,6 +14,8 @@ const base = new Airtable({ apiKey: airtableConfig.apiKey })
 
 function PostsDisplay() {
     const [posts, setPosts] = useState([]);
+    const [author, setAuthor] = useState();
+    const [body, setBody] = useState();
 
     const getPosts = () => {
         base('Posts').select({ view: 'Grid view' }).all()
@@ -24,13 +26,60 @@ function PostsDisplay() {
 
     useEffect(getPosts, []);
 
-    return posts.map((post) => (
+    const addPost = (
+        <form onSubmit={(evt) => {
+            evt.preventDefault();
+            base('Posts').create([
+                {
+                    fields: {
+                        Body: body,
+                        Author: author,
+                    },
+                },
+            ], (err, records) => {
+                if (err) {
+                    console.error(err);
+                    return;
+                }
+                records.forEach((record) => {
+                    console.log(record.getId());
+                });
+            });
+            getPosts();
+        }}>
+            <label>
+                Author:
+                <input type="text" onChange={(e) => setAuthor(e.target.value)} />
+            </label>
+            <label>
+                Body:
+                <input type="text" onChange={(e) => setBody(e.target.value)} />
+            </label>
+            <input
+                type="submit"
+            />
+        </form>
+    );
+
+    const postCollection = posts.map((post) => (
         <Post
             key={post.id}
             author={post.fields.Author}
             body={post.fields.Body}
         />
     ));
+
+    return (
+        <div className="App">
+            <h1 className="Title">Printer</h1>
+            <div>
+                {addPost}
+            </div>
+            <div>
+                {postCollection}
+            </div>
+        </div>
+    );
 }
 
 export default PostsDisplay;
